44e68bacac7e12b3abb75c3fdc147d9e6d8c4c27
Titus Wormer <tituswormer@gmail.com>
2016-01-06 19:36:28 +0100

    Refactor to replace mocha with tape

--- test.js
+++ test.js
@@ -1,188 +1,319 @@
 'use strict';
 
-/* eslint-env mocha */
+/* eslint-env node */
 
 /*
  * Dependencies.
  */
 
 var isBadge = require('./');
-var assert = require('assert');
+var test = require('tape');
 
 /*
  * Tests.
  */
 
-describe('isBadge(url)', function () {
-    it('should fail when not given a string', function () {
-        assert.throws(function () {
+test('isBadge(url)', function (t) {
+    t.throws(
+        function () {
             isBadge(true);
-        }, /Error: is-badge expected string/);
+        },
+        /Error: is-badge expected string/,
+        'should fail when not given a string'
+    );
+
+    t.test('travis', function (st) {
+        st.equal(
+            isBadge('https://travis-ci.org/wooorm/foo.svg'),
+            true,
+            'ok: project'
+        );
+
+        st.equal(
+            isBadge('https://secure.travis-ci.org/wooorm/foo.png'),
+            true,
+            'ok: secure'
+        );
+
+        st.equal(
+            isBadge('https://api.travis-ci.org/wooorm/foo.png?branch=1.1.0'),
+            true,
+            'ok: api'
+        );
+
+        st.equal(
+            isBadge('https://travis-ci.org/wooorm/foo.svg?branch=1.1.1'),
+            true,
+            'ok: branch'
+        );
+
+        st.equal(
+            isBadge('https://travis-ci.org/wooorm/foo'),
+            false,
+            'not ok: w/o extension'
+        );
+
+        st.end();
     });
 
-    it('should work for travis', function () {
-        assert.equal(isBadge(
-            'https://travis-ci.org/wooorm/foo.svg?branch=1.1.1'
-        ), true);
+    t.test('shields.io', function (st) {
+        st.equal(
+            isBadge('https://img.shields.io/travis/joyent/node.svg'),
+            true,
+            'ok'
+        );
 
-        assert.equal(isBadge(
-            'https://travis-ci.org/wooorm/foo.svg'
-        ), true);
+        st.equal(
+            isBadge(
+                'https://img.shields.io/teamcity/http/' +
+                'teamcity.jetbrains.com/s/bt345.svg'
+            ),
+            true,
+            'ok: extensive'
+        );
 
-        assert.equal(isBadge(
-            'https://secure.travis-ci.org/wooorm/foo.png'
-        ), true);
+        st.equal(
+            isBadge('http://img.shields.io/badge/unicorn-approved-ff69b4.svg'),
+            true,
+            'ok: parameters'
+        );
 
-        assert.equal(isBadge(
-            'https://api.travis-ci.org/wooorm/foo.png?branch=1.1.0'
-        ), true);
+        st.equal(
+            isBadge(
+                'http://img.shields.io/badge/unicorn-approved-ff69b4.svg?' +
+                'label=foo'
+            ),
+            true,
+            'ok: GET variabels'
+        );
 
-        assert.equal(isBadge(
-            'https://travis-ci.org/wooorm/foo'
-        ), false);
+        st.equal(
+            isBadge('http://shields.io/'),
+            false,
+            'not ok: w/o project'
+        );
+
+        st.equal(
+            isBadge('http://shields.io/logo.svg'),
+            false,
+            'not ok: w/o service'
+        );
+
+        st.end();
     });
 
-    it('should work for shields', function () {
-        assert.equal(isBadge(
-            'https://img.shields.io/travis/joyent/node.svg'
-        ), true);
+    t.test('david-dm', function (st) {
+        st.equal(
+            isBadge('https://david-dm.org/strongloop/express.svg?style=flat'),
+            true,
+            'ok: flat'
+        );
 
-        assert.equal(isBadge(
-            'https://img.shields.io/teamcity/http/teamcity.jetbrains.com/s/' +
-            'bt345.svg'
-        ), true);
+        st.equal(
+            isBadge(
+                'https://david-dm.org/strongloop/express.svg?' +
+                'style=flat-square'
+            ),
+            true,
+            'ok: flat-square'
+        );
 
-        assert.equal(isBadge(
-            'http://img.shields.io/badge/unicorn-approved-ff69b4.svg'
-        ), true);
+        st.equal(
+            isBadge(
+                'https://david-dm.org/strongloop/express.svg?' +
+                'style=flat-square'
+            ),
+            true,
+            'not ok: w/o extension'
+        );
 
-        assert.equal(isBadge(
-            'http://img.shields.io/badge/unicorn-approved-ff69b4.png' +
-            '?label=foo'
-        ), true);
-
-        assert.equal(isBadge(
-            'http://shields.io/logo.svg'
-        ), false);
+        st.end();
     });
 
-    it('should work for david-dm', function () {
-        assert.equal(isBadge(
-            'https://david-dm.org/strongloop/express.svg?style=flat'
-        ), true);
+    t.test('node-ico', function (st) {
+        st.equal(
+            isBadge('https://nodei.co/npm/peerflix.png?downloads=true'),
+            true,
+            'ok: with downloads'
+        );
 
-        assert.equal(isBadge(
-            'https://david-dm.org/strongloop/express.svg?style=flat-square'
-        ), true);
+        st.equal(
+            isBadge(
+                'https://nodei.co/npm/after.png?downloads=true' +
+                '&downloadRank=true&stars=true'
+            ),
+            true,
+            'ok: w/ downloads, downloadRank, and stars'
+        );
 
-        assert.equal(isBadge(
-            'https://david-dm.org'
-        ), false);
+        st.equal(
+            isBadge('https://nodei.co/nodeico-chrome-screenshot.png'),
+            false,
+            'not ok: w/o provider'
+        );
+
+        st.end();
     });
 
-    it('should work for node-ico', function () {
-        assert.equal(isBadge(
-            'https://nodei.co/npm/peerflix.png?downloads=true'
-        ), true);
+    t.test('inch-ci', function (st) {
+        st.equal(
+            isBadge('https://inch-ci.org/github/wooorm/foo.svg?branch=master'),
+            true,
+            'ok: w/ branch'
+        );
 
-        assert.equal(isBadge(
-            'https://nodei.co/npm/after.png?downloads=true' +
-            '&downloadRank=true&stars=true'
-        ), true);
+        st.equal(
+            isBadge('https://inch-ci.org/github/wooorm/foo.svg'),
+            true,
+            'ok: svg extension'
+        );
 
-        assert.equal(isBadge(
-            'https://nodei.co/nodeico-chrome-screenshot.png'
-        ), false);
+        st.equal(
+            isBadge('https://inch-ci.org/github/wooorm/foo.png'),
+            true,
+            'ok: png extension'
+        );
+
+        st.equal(
+            isBadge(
+                'https://inch-ci.org/github/wooorm/foo.svg?' +
+                'branch=master&style=shields'
+            ),
+            true,
+            'ok: w/ branch and style'
+        );
+
+        st.equal(
+            isBadge('https://inch-ci.org/github/wooorm/foo'),
+            false,
+            'not ok: w/o extension'
+        );
+
+        st.end();
     });
 
-    it('should work for inch-ci', function () {
-        assert.equal(isBadge(
-            'https://inch-ci.org/github/wooorm/foo.svg?branch=master'
-        ), true);
+    t.test('fury.io', function (st) {
+        st.equal(
+            isBadge('http://badge.fury.io/js/engine.io.svg'),
+            true,
+            'ok'
+        );
 
-        assert.equal(isBadge(
-            'https://inch-ci.org/github/wooorm/foo.svg'
-        ), true);
+        st.equal(
+            isBadge('https://badge.fury.io/gh/substack%2Fnode-browserify.svg'),
+            true,
+            'ok: w/ provider'
+        );
 
-        assert.equal(isBadge(
-            'https://inch-ci.org/github/wooorm/foo.png'
-        ), true);
+        st.equal(
+            isBadge('https://badge.fury.io'),
+            false,
+            'not ok: w/o project'
+        );
 
-        assert.equal(isBadge(
-            'https://inch-ci.org/github/wooorm/foo.svg?' +
-            'branch=master&style=shields'
-        ), true);
+        st.equal(
+            isBadge('https://badge.fury.io/gh/substack%2Fnode-browserify'),
+            false,
+            'not ok: w/o extension'
+        );
+
+        st.end();
     });
 
-    it('should work for badge.fury.io', function () {
-        assert.equal(isBadge(
-            'http://badge.fury.io/js/engine.io.svg'
-        ), true);
+    t.test('testling-ci', function (st) {
+        st.equal(
+            isBadge('https://ci.testling.com/substack/dnode.png'),
+            true,
+            'ok'
+        );
 
-        assert.equal(isBadge(
-            'https://badge.fury.io/gh/substack%2Fnode-browserify.svg'
-        ), true);
+        st.equal(
+            isBadge(
+                'https://ci.testling.com/_/images/' +
+                'testling_mission_control.png'
+            ),
+            false,
+            'not ok: w/o project'
+        );
 
-        assert.equal(isBadge(
-            'https://badge.fury.io/gh/substack%2Fnode-browserify'
-        ), false);
+        st.equal(
+            isBadge('https://ci.testling.com/guide/tape'),
+            false,
+            'not ok: w/o extension'
+        );
 
-        assert.equal(isBadge(
-            'https://badge.fury.io/gh/substack/node-browserify.svg'
-        ), false);
+        st.end();
     });
 
-    it('should work for testling-ci', function () {
-        assert.equal(isBadge(
-            'https://ci.testling.com/substack/dnode.png'
-        ), true);
+    t.test('sauce-labs', function (st) {
+        st.equal(
+            isBadge('https://saucelabs.com/buildstatus/vuejs'),
+            true,
+            'ok'
+        );
 
-        assert.equal(isBadge(
-            'https://ci.testling.com/_/images/testling_mission_control.png'
-        ), false);
+        st.equal(
+            isBadge('https://saucelabs.com/browser-matrix/vuejs.svg'),
+            true,
+            'ok: w/ extension'
+        );
 
-        assert.equal(isBadge(
-            'https://ci.testling.com/guide/tape'
-        ), false);
+        st.equal(
+            isBadge(
+                'https://docs.saucelabs.com/images/sauce-labs.bbed5298.png'
+            ),
+            false,
+            'not ok: w/o project'
+        );
+
+        st.equal(
+            isBadge('https://saucelabs.com/home/logo_eventbrite2x.png'),
+            false,
+            'not ok: w/o project (#2)'
+        );
+
+        st.end();
     });
 
-    it('should work for sauce-labs', function () {
-        assert.equal(isBadge(
-            'https://saucelabs.com/buildstatus/vuejs'
-        ), true);
+    t.test('coveralls', function (st) {
+        st.equal(
+            isBadge('https://coveralls.io/repos/jquery/esprima/badge.png'),
+            true,
+            'ok: png'
+        );
 
-        assert.equal(isBadge(
-            'https://saucelabs.com/browser-matrix/vuejs.svg'
-        ), true);
+        st.equal(
+            isBadge('https://coveralls.io/repos/jquery/esprima/badge.svg'),
+            true,
+            'ok: svg'
+        );
 
-        assert.equal(isBadge(
-            'https://docs.saucelabs.com/images/sauce-labs.bbed5298.png'
-        ), false);
+        st.equal(
+            isBadge(
+                'https://coveralls.io/repos/jquery/esprima/badge.svg?' +
+                'branch=master'
+            ),
+            true,
+            'ok: w/ branch'
+        );
 
-        assert.equal(isBadge(
-            'https://saucelabs.com/home/logo_eventbrite2x.png'
-        ), false);
+        st.equal(
+            isBadge(
+                'https://coveralls.io/assets/' +
+                'home_bitbucket-9172913f63c2492acfd0b67d9f0ad404.png'
+            ),
+            false,
+            'not ok: w/o project'
+        );
+
+        st.equal(
+            isBadge('https://coveralls.io/r/google/yapf'),
+            false,
+            'not ok: w/o extension'
+        );
+
+        st.end();
     });
 
-    it('should work for coveralls', function () {
-        assert.equal(isBadge(
-            'https://coveralls.io/repos/jquery/esprima/badge.png'
-        ), true);
-
-        assert.equal(isBadge(
-            'https://coveralls.io/repos/jquery/esprima/badge.svg'
-        ), true);
-
-        assert.equal(isBadge(
-            'https://coveralls.io/repos/jquery/esprima/badge.svg?branch=master'
-        ), true);
-
-        assert.equal(isBadge(
-            'https://coveralls.io/assets/home_bitbucket-9172913f63c2492acfd0b67d9f0ad404.png'
-        ), false);
-
-        assert.equal(isBadge(
-            'https://coveralls.io/r/google/yapf'
-        ), false);
-    });
+    t.end();
 });
